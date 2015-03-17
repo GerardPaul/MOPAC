10a30991cc6cb6133514950d532484f15ce7510a
Gerard Paul P. Labitad <gerardpaul.labitad@outlook.com>
2015-03-17 14:32:29 +0800

    Finished Search screen.

--- app/src/main/java/com/gpplworx/mopac/mopac/MainApp.java
+++ app/src/main/java/com/gpplworx/mopac/mopac/MainApp.java
@@ -2,8 +2,12 @@ package com.gpplworx.mopac.mopac;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class MainApp extends Activity {
@@ -18,6 +22,22 @@ public class MainApp extends Activity {
                 new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,category);
         Spinner spinner = (Spinner)  findViewById(R.id.category);
         spinner.setAdapter(stringArrayAdapter);
+
+        EditText search = (EditText) findViewById(R.id.search_item);
+
+        search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
+    public void performSearch(){
+
+    }
 }
